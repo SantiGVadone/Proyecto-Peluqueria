9869d3f7b4d8f5aa55a8d6d3d6a7784ecd4f9aa6
SantiGVadone <santiagovadone36@gmail.com>
2026-02-23 18:31:40 -0300

    add: updateClient and deleteClient

--- Backend/src/controllers/client.controller.ts
+++ Backend/src/controllers/client.controller.ts
@@ -39,3 +39,25 @@ export const createClient = (req: Request, res: Response) => {
     res.status(201).json(newClient)
 }
 
+//actualizar un cliente existente 
+export const updateClient = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id as string, 10)
+    const clientIndex = clients.findIndex( c => c.id === id) //busca el cliente por id y devuelve la pos 
+    if ( clientIndex === -1 ) {
+        return res.status(404).json({ message: 'Cliente no encontrado'})
+    }
+    const updatedClient = { ...clients[clientIndex], ...req.body }
+    clients[clientIndex] = updatedClient
+    return res.status(200).json({ message: 'Cliente actualizado correctamente', client: updatedClient })
+}
+
+export const deleteClient = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id as string, 10)
+    const clientIndex = clients.findIndex( c => c.id === id) //busca el cliente por id y devuelve la pos 
+    if ( clientIndex === -1 ) {
+        return res.status(404).json({ message: 'Cliente no encontrado'})
+    }
+    clients.splice(clientIndex, 1) //elimina el cliente del array
+    return res.status(200).json({ message: 'Cliente eliminado correctamente'})
+}
+
--- Backend/src/routes/client.routes.ts
+++ Backend/src/routes/client.routes.ts
@@ -8,6 +8,15 @@ const router = Router()
 router.get('/', ClientController.getAllClients)
 
 //Obtener un cliente por ID
- router.get('/:id',ClientController.getClientById)
+router.get('/:id',ClientController.getClientById)
+
+// Crear un nuevo cliente
+router.post('/',ClientController.createClient)
+
+// actualizar un cliente existente 
+//router.patch('/:id',ClientController.updateClient)
+
+// Eliminar un cliente
+//router.delete('/:id',ClientController.deleteClient)
 
 export default router
