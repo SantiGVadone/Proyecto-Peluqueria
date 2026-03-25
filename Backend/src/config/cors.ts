99a439f28fb459f331dd74437224929fa4fb3b4f
SantiGVadone <santiagovadone36@gmail.com>
2026-03-25 19:00:43 -0300

    fix: cors (add Frontend origin)

--- Backend/src/config/cors.ts
+++ Backend/src/config/cors.ts
@@ -1,12 +1,10 @@
-import cors, {CorsOptions} from "cors"
+import cors, { CorsOptions } from 'cors'
 
-const allowedOrigins = [
-    "http://localhost:3000",
-]
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173']
 
 const corsOptions: CorsOptions = {
-    origin: allowedOrigins,
-    credentials: true,
+  origin: allowedOrigins,
+  credentials: true,
 }
 
 export const corsMiddleware = cors(corsOptions)
