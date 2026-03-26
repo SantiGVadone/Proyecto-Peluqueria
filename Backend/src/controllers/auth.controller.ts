e6719e6aec1dcf6565e3de24a7a26b222193206c
SantiGVadone <santiagovadone36@gmail.com>
2026-03-26 18:22:03 -0300

    fix: return of response

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -1,42 +1,52 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 import * as authService from '../services/auth.services'
-import { RegisterBossDTO, RegisterEmployeeDTO, LoginDTO } from '../interfaces/auth.interfaces';
+import {
+  RegisterBossDTO,
+  RegisterEmployeeDTO,
+  LoginDTO,
+} from '../interfaces/auth.interfaces'
 
 export const registerBoss = async (req: Request, res: Response) => {
-    try {
-        const data: RegisterBossDTO = res.locals.validatedBody ?? req.body
-        
-        await authService.registerBossService(data)
-        res.status(201).json({ message: 'Negocio y administrador creados con exito'})
-    } catch (e) {
-        console.log(e)
-        res.status(500).json({message: 'Error al crear el negocio y administrador' })
-    }
-};
+  try {
+    const data: RegisterBossDTO = res.locals.validatedBody ?? req.body
+
+    await authService.registerBossService(data)
+    res
+      .status(201)
+      .json({ message: 'Negocio y administrador creados con exito' })
+  } catch (e) {
+    console.log(e)
+    res
+      .status(500)
+      .json({ message: 'Error al crear el negocio y administrador' })
+  }
+}
 
 export const registerEmployee = async (req: Request, res: Response) => {
-    try{
-        const data: RegisterEmployeeDTO = res.locals.validatedBody ?? req.body
+  try {
+    const data: RegisterEmployeeDTO = res.locals.validatedBody ?? req.body
 
-        await authService.registerEmployeeService(data)
+    await authService.registerEmployeeService(data)
 
-        res.status(201).json({message: 'Usuario creado con exito'})
-    }catch(e){
-        console.log(e)
-        res.status(500).json({ message: 'Error al crear el usuario'})
-    }
+    res.status(201).json({ message: 'Usuario creado con exito' })
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({ message: 'Error al crear el usuario' })
+  }
 }
 
 export const login = async (req: Request, res: Response) => {
-    try {
-        const data: LoginDTO = res.locals.validatedBody ?? req.body
-        // Llamamos al servicio
-        await authService.loginService(data)
-        
-        res.json({message: 'Login exitoso '})
-    } catch (e) {
-        // Si el servicio tira el error de "Credenciales inválidas", cae acá
-        console.log(e)
-        res.status(401).json({message: 'Error en el login'})
-    }
-};
+  try {
+    const data: LoginDTO = res.locals.validatedBody ?? req.body
+    // Llamamos al servicio
+    const response = await authService.loginService(data)
+
+    //const { token, user } = response
+
+    res.json({ message: 'Login exitoso ', response })
+  } catch (e) {
+    // Si el servicio tira el error de "Credenciales inválidas", cae acá
+    console.log(e)
+    res.status(401).json({ message: 'Error en el login' })
+  }
+}
--- Backend/src/services/auth.services.ts
+++ Backend/src/services/auth.services.ts
@@ -1,63 +1,62 @@
-import {pool} from '../config/db';
-import { LoginDTO, RegisterBossDTO, RegisterEmployeeDTO } from '../interfaces/auth.interfaces';
-import * as authRepo from '../repository/auth.repository';
+import { pool } from '../config/db'
+import {
+  LoginDTO,
+  RegisterBossDTO,
+  RegisterEmployeeDTO,
+} from '../interfaces/auth.interfaces'
+import * as authRepo from '../repository/auth.repository'
 import { comparePassword, hashPassword } from '../utils/auth.utils'
 import jwt from 'jsonwebtoken'
 
-export const registerBossService = async (data: RegisterBossDTO ) => {
-    const client = await pool.connect();
-    try {
-        await client.query('BEGIN');
+export const registerBossService = async (data: RegisterBossDTO) => {
+  const client = await pool.connect()
+  try {
+    await client.query('BEGIN')
 
-        // crear el negocio y obtener el id
-        const businessId = await authRepo.createBusinessRepo(client, data);
+    // crear el negocio y obtener el id
+    const businessId = await authRepo.createBusinessRepo(client, data)
 
-        // hasheo la pass 
-        const hashedPassword = await hashPassword(data.password);
-
-        // crear el employee con rol boss
-        await authRepo.createBossRepo(client, data, businessId, hashedPassword);
-
-        await client.query('COMMIT');
-    } catch (error) {
-        await client.query('ROLLBACK');
-        throw error;
-    } finally {
-        client.release();
-    }
-};
-
-export const registerEmployeeService = async (data: RegisterEmployeeDTO) => {
-
+    // hasheo la pass
     const hashedPassword = await hashPassword(data.password)
 
-    await authRepo.createEmployeeRepo(data, hashedPassword)
-    
+    // crear el employee con rol boss
+    await authRepo.createBossRepo(client, data, businessId, hashedPassword)
+
+    await client.query('COMMIT')
+  } catch (error) {
+    await client.query('ROLLBACK')
+    throw error
+  } finally {
+    client.release()
+  }
 }
 
+export const registerEmployeeService = async (data: RegisterEmployeeDTO) => {
+  const hashedPassword = await hashPassword(data.password)
+
+  await authRepo.createEmployeeRepo(data, hashedPassword)
+}
 
 export const loginService = async (data: LoginDTO) => {
-    const user = await authRepo.getUserByEmailRepo(data.email);
-    if (!user) throw new Error('Email no registrado');
+  const user = await authRepo.getUserByEmailRepo(data.email)
+  if (!user) throw new Error('Email no registrado')
 
-    const isMatch = await comparePassword(data.password, user.password);
-    if (!isMatch) throw new Error('Contraseña incorrecta o usuario no registrado');
+  const isMatch = await comparePassword(data.password, user.password)
+  if (!isMatch) throw new Error('Contraseña incorrecta o usuario no registrado')
 
-    // Generamos el token con la info de sesión
-    const token = jwt.sign(
-        { id: user.id,
-          role: user.role, 
-          business_id: user.business_id 
-        },
-        process.env.JWT_SECRET || 'secret_provisorio',
-        { expiresIn: '8h' }
-    );
+  // Generamos el token con la info de sesión
+  const token = jwt.sign(
+    { id: user.id, role: user.role, business_id: user.business_id },
+    process.env.JWT_SECRET || 'secret_provisorio',
+    { expiresIn: '8h' },
+  )
 
-    return {
-        token,
-        user: { 
-            name: user.name, 
-            role: user.role, 
-            business_id: user.business_id }
-    };
-};
+  return {
+    token,
+    user: {
+      name: user.name,
+      role: user.role,
+      business_id: user.business_id,
+    },
+  }
+}
